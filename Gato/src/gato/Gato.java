a97d7c11dd35498f5c112908e0784847f491939e
FabianEsteban <fabian.esteban.0@gmail.com>
2017-09-26 23:04:10 -0300

    Add files via upload

---
+++ Gato/src/gato/Gato.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gato;
+
+/**
+ *
+ * @author Fabián
+ */
+public class Gato {
+    
+    public static void main(String[] args) {
+        juego j = new juego();
+        j.setVisible(true);
+    }
+    
+}
